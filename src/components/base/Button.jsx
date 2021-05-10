972e11ef5d532f5c966d60bb6f1e4e2819261e6d
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 17:44:32 -0500

    regular classes 4 Button Component

--- src/components/base/Button.jsx
+++ src/components/base/Button.jsx
@@ -1,8 +1,9 @@
 export const Button = ({ btnType = "button", type = "regular", title, handleClick }) => {
   const classesPerType = {
-    regular: "",
+    regular:
+      "group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-gray-50 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200",
     primary:
-      "group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+      "group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
   };
 
   return (
