d595c289a73a5eab44ef1a0a48557795f1f218b1
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 15:33:30 -0500

    FormItem component updated test

--- src/tests/components/base/FormItem.test.js
+++ src/tests/components/base/FormItem.test.js
@@ -1,13 +1,39 @@
-import { render } from '@testing-library/react'
-import { FormItem } from '../../../components/base/FormItem'
+import { cleanup, screen, render } from "@testing-library/react";
+import { FormItem } from "../../../components/base/FormItem";
 
-test('renders <FormItem /> and assigns the htmlFor as it should', () => {
-  const label = 'Test'
-  const { getByLabelText } = render(
-    <FormItem label={label}>
-      <input id="test" />
+afterEach(() => cleanup());
+
+function setup(isSrOnly = false, required = false) {
+  const label = "Test";
+  const utils = render(
+    <FormItem label={label} isSrOnly={isSrOnly} required={required}>
+      <input data-testid="test" id="test" />
     </FormItem>
-  )
+  );
 
-  expect(getByLabelText(label)).not.toBeNull()
-})
+  return {
+    label,
+    ...utils,
+  };
+}
+
+function setupWithNoRequiredFields() {
+  const utils = setup(true);
+  return utils;
+}
+
+function setupWithRequiredFields() {
+  const utils = setup(false, true);
+  return utils;
+}
+
+test("renders <FormItem /> assigns the htmlFor and renders the label hidden.", () => {
+  const { getByLabelText, label, getByTestId } = setupWithNoRequiredFields();
+  expect(getByLabelText(label)).not.toBeNull();
+  expect(getByTestId("test")).not.toBeNull();
+});
+
+test("renders <FormItem /> with the field marked as required.", () => {
+  const { container } = setupWithRequiredFields();
+  expect(container.querySelector('.text-red-400')).not.toBeNull()
+});
