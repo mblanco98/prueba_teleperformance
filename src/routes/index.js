d840fb7b665569ec6349608d0106c4008a11d6ba
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 20:17:55 -0500

    using Notification component on Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,23 @@
+import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Layout } from '../components/Layout'
 import { Login, Register } from '../pages'
+import { Notification } from "../components/base/Notification";
 
-export const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Layout>
-        <Route exact path='/' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Layout>
-      {/* <Route exact path='/profile' component={Profile} /> */}
-    </Switch>
-  </BrowserRouter>
-)
+export const App = () => {
+  const notification = useSelector(state => state.notification)
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Layout>
+          <Notification {...notification} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Layout>
+        {/* <Route exact path='/profile' component={Profile} /> */}
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
