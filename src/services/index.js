70e27a08a2001ce2d8cea02d410193a8cd4fa891
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 20:19:33 -0500

    login service

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,30 @@
-import { $axios } from '../config/apiConfig'
+import { $axios, config } from "../config/apiConfig";
+import { registerUserMockMessage } from "../mocks";
 
 export const registerUser = async (data) => {
   try {
-    const res = await $axios.post('/register')
-    return res
+    const params = new URLSearchParams();
+    const keys = Object.keys(data);
+
+    keys.forEach((key) => {
+      params.append(key, data[key]);
+    });
+
+    const res = await $axios.post("/api", params, {
+      headers: config.headersPost,
+    });
+
+    return res;
   } catch (err) {
-    console.error(err)
-    return err.message
+    console.error(err);
   }
-}
+};
+
+export const checkNIT = async ({ nit }) => {
+  try {
+    const { data } = await $axios.get(`${config.baseUrl}/auth`);
+    return { ...registerUserMockMessage(data.includes(Number(nit))), data: nit };
+  } catch (err) {
+    console.error(err);
+  }
+};
