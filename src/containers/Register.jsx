b4dcd9f4ea2ee902ea01e753009525e802ca9ed7
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 20:45:06 -0500

    validating query on register page

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,9 +1,22 @@
 import { connect } from 'react-redux'
+import { useLocation } from 'react-router'
 import { RegisterCard } from '../components/Register'
 import { registerRequest } from '../store/actions'
+import { useEffect } from 'react'
 
 const mapDispatchToProps = {
   registerRequest,
 }
 
-export const Register = connect(null, mapDispatchToProps)(RegisterCard);
+const RegisterWrapper = ({ history, registerRequest }) => {
+  const location = useLocation();
+  useEffect(() => {
+    if (location.search.length <= 3) {
+      history.replace('/')
+    }
+  }, [location.search, history])
+  return <RegisterCard history={history} registerRequest={registerRequest} />
+}
+
+
+export const Register = connect(null, mapDispatchToProps)(RegisterWrapper);
