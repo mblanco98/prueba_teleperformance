cd44acf5e1989e138454791d14ac28b88fcd2ca2
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 22:08:28 -0500

    showing field depending of IdType

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -6,8 +6,10 @@ import { FormItem } from "./base/FormItem";
 import { InputText } from "./base/InputText";
 import { InputNumber } from "./base/InputNumber";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom'
-import { CheckBox } from './base/CheckBox'
+import { Link } from "react-router-dom";
+import { CheckBox } from "./base/CheckBox";
+import { Select } from "./base/Select";
+import { countriesAvailable, documentTypes } from "../data";
 
 export const RegisterCard = ({ registerRequest }) => {
   const [form, setForm] = useState({
@@ -34,11 +36,17 @@ export const RegisterCard = ({ registerRequest }) => {
   };
 
   const handleSubmit = () => {
-    console.log(form)
+    registerRequest(form)
   };
 
+  const IdTypeIsNitOrForeignerId =
+    form.identificationType === "nit" ||
+    form.identificationType === "foreigner ID"
+      ? true
+      : false;
+
   return (
-    <Card title="Registro">
+    <Card title="">
       <header className="p-2 ml-4 mr-4 bg-yellow-100">
         <p className="text-xs text-gray-500 ml-2">
           The fields marked with <b className="text-red-400">*</b> are required.
@@ -46,52 +54,70 @@ export const RegisterCard = ({ registerRequest }) => {
       </header>
       <Form handleOnSubmit={handleSubmit}>
         <div className="grid grid-cols-6 gap-3">
+          
+          {IdTypeIsNitOrForeignerId && (
+            <div className="col-span-6">
+              <FormItem label="Company name" required>
+                <InputText
+                  inputType="text"
+                  inputName="company-name"
+                  inputValue={form.companyName}
+                  handleOnChange={(v) => handleInputChange("companyName", v)}
+                />
+              </FormItem>
+            </div>
+          )}
+
+          {!IdTypeIsNitOrForeignerId && (
+            <>
+              <div className="col-span-6 sm:col-span-2">
+                <FormItem label="First name" required>
+                  <InputText
+                    inputType="text"
+                    inputName="first-name"
+                    inputValue={form.firstName}
+                    handleOnChange={(v) => handleInputChange("firstName", v)}
+                  />
+                </FormItem>
+              </div>
+              <div className="col-span-6 sm:col-span-4">
+                <FormItem label="Second name" required>
+                  <InputText
+                    inputType="text"
+                    inputName="second-name"
+                    inputValue={form.secondName}
+                    handleOnChange={(v) => handleInputChange("secondName", v)}
+                  />
+                </FormItem>
+              </div>
+              <div className="col-span-6 sm:col-span-2">
+                <FormItem label="First last name" required>
+                  <InputText
+                    inputType="text"
+                    inputName="first-last-name"
+                    inputValue={form.firstLastName}
+                    handleOnChange={(v) =>
+                      handleInputChange("firstLastName", v)
+                    }
+                  />
+                </FormItem>
+              </div>
+              <div className="col-span-6 sm:col-span-4">
+                <FormItem label="Second last name" required>
+                  <InputText
+                    inputType="text"
+                    inputName="second-last-name"
+                    inputValue={form.secondLastName}
+                    handleOnChange={(v) =>
+                      handleInputChange("secondLastName", v)
+                    }
+                  />
+                </FormItem>
+              </div>
+            </>
+          )}
+
           <div className="col-span-6">
-            <FormItem label="Company name" required>
-              <InputText
-                inputName="company-name"
-                inputValue={form.companyName}
-                handleOnChange={(v) => handleInputChange("companyName", v)}
-              />
-            </FormItem>
-          </div>
-          <div className="col-span-6 sm:col-span-2">
-            <FormItem label="First name" required>
-              <InputText
-                inputName="first-name"
-                inputValue={form.firstName}
-                handleOnChange={(v) => handleInputChange("firstName", v)}
-              />
-            </FormItem>
-          </div>
-          <div className="col-span-6 sm:col-span-4">
-            <FormItem label="Second name" required>
-              <InputText
-                inputName="second-name"
-                inputValue={form.secondName}
-                handleOnChange={(v) => handleInputChange("secondName", v)}
-              />
-            </FormItem>
-          </div>
-          <div className="col-span-6 sm:col-span-2">
-            <FormItem label="First last name" required>
-              <InputText
-                inputName="first-last-name"
-                inputValue={form.firstLastName}
-                handleOnChange={(v) => handleInputChange("firstLastName", v)}
-              />
-            </FormItem>
-          </div>
-          <div className="col-span-6 sm:col-span-4">
-            <FormItem label="Second last name" required>
-              <InputText
-                inputName="second-last-name"
-                inputValue={form.secondLastName}
-                handleOnChange={(v) => handleInputChange("secondLastName", v)}
-              />
-            </FormItem>
-          </div>
-          <div className="col-span-6 sm:col-span-5">
             <FormItem label="Email" required>
               <InputText
                 inputType="email"
@@ -101,7 +127,16 @@ export const RegisterCard = ({ registerRequest }) => {
               />
             </FormItem>
           </div>
-          <div className="col-span-2">
+          <div className="col-span-6">
+            <FormItem label="Country" required>
+              <Select
+                inputValue={form.countryState}
+                options={countriesAvailable}
+                handleOnChange={(v) => handleInputChange("countryState", v)}
+              />
+            </FormItem>
+          </div>
+          <div className="col-span-6 sm:col-span-2">
             <FormItem label="Cellphone number" required>
               <InputNumber
                 inputName="cellphone-number"
@@ -121,11 +156,11 @@ export const RegisterCard = ({ registerRequest }) => {
               />
             </FormItem>
           </div>
-          <div className="col-span-2">
+          <div className="col-span-6 sm:col-span-2">
             <FormItem label="Identification type" required>
-              <InputText
-                inputName="identification-type"
+              <Select
                 inputValue={form.identificationType}
+                options={documentTypes}
                 handleOnChange={(v) =>
                   handleInputChange("identificationType", v)
                 }
