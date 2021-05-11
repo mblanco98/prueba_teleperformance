54cca477ce7a2af25683d66e33211017e23598ae
manAbl <manuel.22.lamas@gmail.com>
2021-05-11 12:10:43 -0500

    adding devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { reducer } from "./reducers";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
