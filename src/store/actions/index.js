d8cce902b9fe1421198fada7bc4224c1dab7fe5f
manAbl <manuel.22.lamas@gmail.com>
2021-05-10 20:19:19 -0500

    notifications actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,13 +1,34 @@
 import * as types from '../actionTypes'
-import { registerUser } from '../../services'
+import { checkNIT, registerUser } from '../../services'
 
 export const registerRequest = payload => {
   return async dispatch => {
-    const res = await registerUser(payload)
-    console.log(res)
+    await registerUser(payload)
     dispatch({
       type: types.REGISTER_USER,
       payload,
     });
+  }
+}
+
+export const loginRequest = payload => {
+  return async dispatch => {
+    const { message, code } = await checkNIT(payload)
+    
+    dispatch(toggleNotification({
+      title: code,
+      visible: true,
+      description: message,
+      type: code === '' ? 'info' : 'error',
+    }))
+
+    return !!code
+  }
+}
+
+export const toggleNotification = payload => {
+  return {
+    type: types.TOGGLE_NOTIFICATION,
+    payload,
   }
 }
